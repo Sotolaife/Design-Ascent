2ce47e0b60f9275c5d5795ac6129c00987867809
Ifeoluwa Sotola <sotolaifeoluwa@gmail.com>
2025-11-21 19:47:37 +0000

    feat: ui added

--- src/components/landing/header.tsx
+++ src/components/landing/header.tsx
@@ -7,14 +7,14 @@ export default function Header() {
     <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="container mx-auto flex h-16 items-center justify-between px-4">
         <Link href="/" className="flex items-center gap-3">
+        {/* <div className="h-32 w-32 object-contain">
           <Image
             src="https://i.ibb.co/DDwd0dLh/IMG-20251121-WA0006.jpg"
             alt="Olutola Studios Logo"
-            width={40}
-            height={40}
-            className="h-10 w-10 object-contain"
+            fill            
             unoptimized
           />
+          </div> */}
           <span className="font-headline text-lg sm:text-xl font-bold">Olutola Studios</span>
         </Link>
         <Button asChild className="bg-accent text-accent-foreground hover:bg-accent/90 px-4 py-2">
